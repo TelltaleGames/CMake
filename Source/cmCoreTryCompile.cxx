ecd1b3d2716cf70a034c8d3afc84b8e1d2a6cd2c
David Brady <dbrady@telltalegames.com>
2013-02-04 12:27:12 -0800

    Quick hack to allow fixing up of try_compile targets

--- Source/cmCoreTryCompile.cxx
+++ Source/cmCoreTryCompile.cxx
@@ -353,22 +353,26 @@ int cmCoreTryCompile::TryCompileCode(std::vector<std::string> const& argv)
       fprintf(fout, "SET(CMAKE_POSITION_INDEPENDENT_CODE \"ON\")\n");
       }
 
+    /* Use a random file name to avoid rapid creation and deletion
+       of the same executable name (some filesystems fail on that).  */
+    sprintf(targetNameBuf, "cmTryCompileExec%u",
+            cmSystemTools::RandomSeed());
+    targetName = targetNameBuf;
+    
+    fprintf(fout, "SET(CMAKE_TRY_COMPILE_TARGET %s)\n", targetName);
+
     /* Put the executable at a known location (for COPY_FILE).  */
     fprintf(fout, "SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY \"%s\")\n",
             this->BinaryDirectory.c_str());
     /* Create the actual executable.  */
-    fprintf(fout, "ADD_EXECUTABLE(%s \"%s\")\n", targetName, source.c_str());
-    if (useOldLinkLibs)
+    fprintf(fout, "ADD_EXECUTABLE(${CMAKE_TRY_COMPILE_TARGET} \"%s\")\n", source.c_str());
+    fprintf(fout, "TARGET_LINK_LIBRARIES(${CMAKE_TRY_COMPILE_TARGET} ${LINK_LIBRARIES})\n");
+
+    if(this->Makefile->GetDefinition("CMAKE_TRY_COMPILE_FIXUP")!=0)
       {
-      fprintf(fout,
-              "TARGET_LINK_LIBRARIES(%s ${LINK_LIBRARIES})\n",targetName);
+      fprintf(fout, this->Makefile->GetDefinition("CMAKE_TRY_COMPILE_FIXUP"));
       }
-    else
-      {
-      fprintf(fout, "TARGET_LINK_LIBRARIES(%s %s)\n",
-              targetName,
-              libsToLink.c_str());
-      }
+
     fclose(fout);
     projectName = "CMAKE_TRY_COMPILE";
     // if the source is not in CMakeTmp
